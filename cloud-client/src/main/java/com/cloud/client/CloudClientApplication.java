070820ab3287f61bf87265b64b7dd7e6a93a5433
“ <luckymxp@163.com>
2020-07-07 11:01:34 +0800

    服务方，注册中心

---
+++ cloud-client/src/main/java/com/cloud/client/CloudClientApplication.java
@@ -0,0 +1,22 @@
+package com.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 
+ * @author lucky
+ * @date 2020/7/7
+ **/
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientApplication.class, args);
+    }
+
+}
